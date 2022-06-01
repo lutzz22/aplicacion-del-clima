9cb76643ce868b530245d631ecd63267fe85f769
luaramb <arambarrilucia@gmail.com>
2022-06-01 19:22:24 -0300

    segundo commit

--- js/index.js
+++ js/index.js
@@ -2,10 +2,16 @@
 
 const peticionClima = async() => {
     const ciudad = document.querySelector('#ciudad')
-    const ciudad2 = ciudad.value 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad2}&appid=399150bee6e3ef041d7c322ec14534a6`)
+    let ciudad2 = ciudad.value 
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad2}&appid=399150bee6e3ef041d7c322ec14534a6&units=metric&lang=sp`)
     const clima = await response.json()
     console.log(clima)
     document.querySelector('#texto-ciudad').innerText = clima.name
-    // nombreCiudad.innerText = clima.name
+    document.querySelector('#temperatura').textContent = (`${clima.main.temp}ºC`)
+    document.querySelector('#pronostico').textContent = clima.weather[0].description
+    document.querySelector('#humedad').textContent = (`${clima.main.humidity}%`)
+    document.querySelector('#viento').textContent = (`${clima.wind.speed} km/h`)
+    const icono = document.querySelector('#icono')
+    icono.setAttribute('src', `http://openweathermap.org/img/w/${clima.weather[0].icon}.png`)
+    ciudad.value = ''
 }
