b310500b9a3723dd01dc1780cc6f6a770705e912
luaramb <arambarrilucia@gmail.com>
2022-05-31 23:18:01 -0300

    [primer commit]

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
 // Tu codigo JS va acá
+
+const peticionClima = async() => {
+    const ciudad = document.querySelector('#ciudad')
+    const ciudad2 = ciudad.value 
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad2}&appid=399150bee6e3ef041d7c322ec14534a6`)
+    const clima = await response.json()
+    console.log(clima)
+    document.querySelector('#texto-ciudad').innerText = clima.name
+    // nombreCiudad.innerText = clima.name
+}
