555537a76e8f7bf518d4032779ef3232e21aaf09
luaramb <arambarrilucia@gmail.com>
2022-06-06 19:56:41 -0300

    app terminada

--- js/index.js
+++ js/index.js
@@ -1,17 +1,24 @@
 // Tu codigo JS va acá
 
 const peticionClima = async() => {
-    const ciudad = document.querySelector('#ciudad')
-    let ciudad2 = ciudad.value 
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad2}&appid=399150bee6e3ef041d7c322ec14534a6&units=metric&lang=sp`)
-    const clima = await response.json()
-    console.log(clima)
-    document.querySelector('#texto-ciudad').innerText = clima.name
-    document.querySelector('#temperatura').textContent = (`${clima.main.temp}ºC`)
-    document.querySelector('#pronostico').textContent = clima.weather[0].description
-    document.querySelector('#humedad').textContent = (`${clima.main.humidity}%`)
-    document.querySelector('#viento').textContent = (`${clima.wind.speed} km/h`)
-    const icono = document.querySelector('#icono')
-    icono.setAttribute('src', `http://openweathermap.org/img/w/${clima.weather[0].icon}.png`)
-    ciudad.value = ''
+   try {
+        const myKey = '399150bee6e3ef041d7c322ec14534a6'
+        const ciudad = document.querySelector('#ciudad')
+        let ciudad2 = ciudad.value 
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad2}&appid=${myKey}&units=metric&lang=es`)
+        const clima = await response.json()
+        console.log(clima)
+        document.querySelector('#texto-ciudad').innerText = clima.name
+        document.querySelector('#temperatura').textContent = (`${clima.main.temp}ºC`)
+        document.querySelector('#pronostico').textContent = clima.weather[0].description
+        document.querySelector('#humedad').textContent = (`${clima.main.humidity}%`)
+        document.querySelector('#viento').textContent = (`${clima.wind.speed} km/h`)
+        const icono = document.querySelector('#icono')
+        icono.setAttribute('src', `http://openweathermap.org/img/w/${clima.weather[0].icon}.png`)
+    } catch(error){
+        alert('No pudimos identificar la ciudad')
+    } finally {
+        ciudad.value = ''
+    } 
 }
+
